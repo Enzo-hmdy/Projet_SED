6c0d738c1127308396ff34673389e67373ffb35c
enzo.hmdy <enzo.hmdy@gmail.com>
2022-01-17 10:15:50 -0500

    Print averegae wait

--- Project.py
+++ Project.py
@@ -21,5 +21,25 @@ N = ciw.create_network(
              [0, 0, 1, 0],
              [0, 0, 0, 1],
              [0, B/F, 0, 0]],
-    number_of_servers=[1, 1, 1, 1]
+    number_of_servers=[4, 4, 4, 4]
 )
+average_waits = []
+
+for trial in range(10):
+    print("pouet")
+    ciw.seed(trial)
+
+    Q = ciw.Simulation(N)
+
+    Q.simulate_until_max_time(1640)
+
+    recs = Q.get_all_records()
+
+    waits = [r.waiting_time for r in recs if r.arrival_date >
+             100 and r.arrival_date < 1540]
+
+    mean_wait = sum(waits) / len(waits)
+    print(" i : ", mean_wait)
+    average_waits.append(mean_wait)
+
+print(sum(average_waits) / len(average_waits))
